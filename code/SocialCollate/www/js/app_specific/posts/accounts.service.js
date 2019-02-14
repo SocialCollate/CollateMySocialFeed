3a9afc1ff2ea21659f08302f687aeae5287b0c17
Klingon <klingonwarrior2008@gmail.com>
2019-02-14 13:40:15 +0000

    added accounts service

--- code/SocialCollate/www/js/app_specific/posts/accounts.service.js
+++ code/SocialCollate/www/js/app_specific/posts/accounts.service.js
@@ -38,6 +38,43 @@
             }
             //now ACCOUNTS is updated.
         }
+
+        function storeAccount(account) {
+            //store new/update old account in local storage.
+            var arrayLength = ACCOUNTS.length;
+        
+            for (var i = 0; i < arrayLength; i++) {
+        
+                if (ACCOUNTS[i].account_num == account.account_num) {
+        
+                    ACCOUNTS[i].access_token = account.access_token;
+                    ACCOUNTS[i].expiry = account.expiry;
+                    ACCOUNTS[i].time_created = account.time_created;
+        
+                } else {
+                    ACCOUNTS.push(account);
+                }
+                storeLocalAccounts()
+            }
+        }
+        
+        
+        function storeLocalAccounts() {
+            var arrayLength = ACCOUNTS.length;
+            var string = "";
+        
+            for (var i = 0; i < arrayLength; i++) {
+        
+                string += ACCOUNTS[i].account_num + ",";
+                string += ACCOUNTS[i].access_token + ",";
+                string += ACCOUNTS[i].expiry + ",";
+                string += ACCOUNTS[i].time_created + ";";
+        
+            }
+        
+            window.localStorage.setItem("accounts",string);
+        }
+
         return service;
 
     }

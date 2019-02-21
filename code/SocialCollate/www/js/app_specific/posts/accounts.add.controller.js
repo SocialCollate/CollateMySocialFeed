857bce20f5074f7311c030954c60adf62a30301d
Klingon <klingonwarrior2008@gmail.com>
2019-02-21 12:34:27 +0000

    fixed add account code

--- code/SocialCollate/www/js/app_specific/posts/accounts.add.controller.js
+++ code/SocialCollate/www/js/app_specific/posts/accounts.add.controller.js
@@ -24,13 +24,18 @@
         
 
         /*LOGIN FB*/ 
-        vm.FBLogin = function(account){
+        vm.FBLogin = function(){
             $cordovaOauth.facebook("954844384905992", ["user_posts"]).then(function(result) {
-                // results
-                account.access_token = result.access_token;
-                account.expiry = result.expires;
-                account.time_created = Date.now();
-                storeAccount(account)
+                //results
+                let account = {
+                    account_num : ACCOUNTS[ACCOUNT.length-1].account_num,
+                    platform_name: "facebook",
+                    access_token : result.access_token,
+                    expiry : result.expires,
+                    time_created : Date.now()
+                }
+                
+                storeAccount(account);
 
                 //search by account_num
 

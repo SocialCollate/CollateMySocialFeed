4e826760e2b484878274d2d411b471626a5c4fe2
StevoJD <44926352+StevoJD@users.noreply.github.com>
2019-02-21 14:34:35 +0000

    Update accounts.add.controller.js

--- code/SocialCollate/www/js/app_specific/posts/accounts.add.controller.js
+++ code/SocialCollate/www/js/app_specific/posts/accounts.add.controller.js
@@ -43,7 +43,7 @@
                     time_created : Date.now()
                 }
                 
-                storeAccount(account);
+                vm.storeAccount(account);
 
                 //search by account_num
 
@@ -78,7 +78,7 @@
             account.expiry = result.expires;
             account.time_created = Date.now();
             */
-            storeAccount(account);
+            vm.storeAccount(account);
 
             console.log(account);
 
